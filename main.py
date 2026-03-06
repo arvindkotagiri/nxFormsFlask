bd66d06502cc012b5ee324333978a22833be9a57
K S Aravind Kumar <akmanutd01@yahoo.in>
2026-03-06 00:42:47 -0600

    Modify app.run to use environment variable for port
    Updated the main application to run on a dynamic port from environment variables.

--- main.py
+++ main.py
@@ -23,5 +23,10 @@ def create_app():
 
 app = create_app()
 
+# if __name__ == "__main__":
+#     app.run(port=5050, debug=False, threaded=True)
+
+
 if __name__ == "__main__":
-    app.run(port=5050, debug=False, threaded=True)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=False)
